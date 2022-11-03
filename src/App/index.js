71f0ab8033de6a28cef54e53daa010559dac8504
Juan Cruz Bonelli <juancbonelli@gmail.com>
2022-11-03 11:41:55 -0300

    TODO-Machine: improve the logic and the file structure

---
+++ src/App/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { AppUI } from './AppUI';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar curso de react', completed: false },
+  { text: 'tomar curso photoshop', completed: false },
+];
+
+function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+
+  const totalTodos = todos.length;
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <AppUI
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  );
+}
+
+export default App;
